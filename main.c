d4f0cd6458ac9cfe56f3cdae1191f8822400efcf
skykim33 <kimhn24@sookmyung.ac.kr>
2026-09-17 16:21:44 +0900

    실습 2

--- main.c
+++ main.c
@@ -2,16 +2,14 @@
 
 int main (void)
 {
-    int input_int;
-    float input_float;
+    char c;
+    int i;
 
-    printf("enter an integer: ");
-    scanf("%d", &input_int);
+    printf("input a number :");
+    scanf("%c", &c);
 
-    printf("enter a float: ");
-    scanf("%f", &input_float);
-
-    printf("integer : %d, float : %f\n", input_int, input_float);
+    i = c - '0';  // Convert character to integer
+    printf("The input number is %i\n", i);
 
     return 0;
 }
